aa444ac9eb7f353c8d1d27691c4301cdea9c3ea6
Mark Kennedy <mark.kennedy@akqa.com>
2015-01-29 10:25:54 -0500

    Fixing tests in phantomjs by moving 'this' binding polyfill to test file

--- tests/tests.js
+++ tests/tests.js
@@ -19,6 +19,33 @@ require.config({
 require([
     'tests/scroll-tests'
 ], function() {
+
+    // add polyfill for Function.bind for phantomJS (running tests headlessly)
+    if (!Function.prototype.bind) {
+        Function.prototype.bind = function (oThis) {
+            if (typeof this !== 'function') {
+                // closest thing possible to the ECMAScript 5
+                // internal IsCallable function
+                throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable');
+            }
+
+            var aArgs = Array.prototype.slice.call(arguments, 1),
+                fToBind = this,
+                fNOP = function () {},
+                fBound = function () {
+                    return fToBind.apply(this instanceof fNOP && oThis
+                            ? this
+                            : oThis,
+                        aArgs.concat(Array.prototype.slice.call(arguments)));
+                };
+
+            fNOP.prototype = this.prototype;
+            fBound.prototype = new fNOP();
+
+            return fBound;
+        };
+    }
+
     QUnit.config.requireExpects = true;
     QUnit.start();
 });
