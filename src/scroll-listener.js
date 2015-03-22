39e76714198a31b47e4f2109e1d6a5664459a819
Mark Kennedy <mark.kennedy@akqa.com>
2015-03-22 14:17:43 -0400

    Added first set of scroll listener tests

--- src/scroll-listener.js
+++ src/scroll-listener.js
@@ -121,10 +121,24 @@ ScrollListener.prototype = {
     getElementMinYPos: function () {
         if (!this._elementMinYPos) {
             var // must add viewport scroll position to getBoundingClientRect get a constant, absolute value
-                elementYPos = this.options.el.getBoundingClientRect().top + this.getContainerScrollYPos();
+                elementYPos = this.getElementYPos();
             this._elementMinYPos = elementYPos - this.getViewportHeight() - this.options.offsetTop;
         }
         return this._elementMinYPos;
+    },
+
+    /**
+     * Returns the amount of pixels the element is from the top of the container.
+     * @returns {number}
+     */
+    getElementYPos: function () {
+        var container = this.options.container,
+            elementYPos = container.scrollTop;
+        if (container === document) {
+            // must add viewport scroll position to getBoundingClientRect get a constant, absolute value
+            elementYPos = this.options.el.getBoundingClientRect().top + this.getContainerScrollYPos();
+        }
+        return elementYPos;
     },
 
     /**
--- tests/scroll-listener-tests.js
+++ tests/scroll-listener-tests.js
@@ -1,6 +1,7 @@
 'use strict';
 var sinon = require('sinon');
 var assert = require('assert');
+var TestUtils = require('test-utils');
 var ScrollListener = require('../src/scroll-listener');
 
 describe('Scroll Listener', function () {
@@ -20,19 +21,61 @@ describe('Scroll Listener', function () {
         requestAnimationFrameStub.restore();
     });
 
-    it('should fire onEnterView callback when container\'s scrollTop position + its height reaches the top of the element', function() {
+    it('should not fire onEnterView callback if element is out of view upon instantiation', function() {
         var outerEl = document.createElement('div');
         var innerEl = document.createElement('div');
         outerEl.appendChild(innerEl);
         document.body.appendChild(outerEl);
-        // setup to be "scrollable"
-        outerEl.style.overflow = 'hidden';
+        // setup outer element
+        outerEl.style.overflow = 'hidden'; // make it scrollable
         outerEl.style.height = '150px';
+        outerEl.style.width = '10px';
         // inner element
-        innerEl.style.height = '600px';
+        innerEl.style.top = '300px'; // move element out of view
+        innerEl.style.width = '10px';
+        innerEl.style.height = '150px';
         // setup current scroll position
-        outerEl.scrollTop = 100;
-        var scrollListener = new ScrollListener({el: outerEl});
+        outerEl.scrollTop = 0;
+        var onEnterSpy = sinon.spy();
+        var scrollListener = new ScrollListener({
+            el: outerEl,
+            container: outerEl,
+            onEnter: onEnterSpy
+        });
+        assert.equal(onEnterSpy.callCount, 0, 'onEnter callback was not fired upon instantiation because element is out of view');
+        document.body.removeChild(outerEl);
+    });
+
+    it('should fire onEnter callback when container\'s scrollTop position + its height reaches the top of the element', function() {
+        var outerEl = document.createElement('div');
+        var innerEl = document.createElement('div');
+        outerEl.appendChild(innerEl);
+        document.body.appendChild(outerEl);
+        // setup outer element
+        outerEl.style.overflow = 'hidden'; // make it scrollable
+        outerEl.style.height = '150px';
+        outerEl.style.width = '10px';
+        // inner element
+        innerEl.style.top = '300px'; // move element out of view
+        innerEl.style.left = '0';
+        innerEl.style.width = '10px';
+        innerEl.style.height = '150px';
+        innerEl.style.position = 'relative';
+        // setup current scroll position
+        outerEl.scrollTop = 0;
+        var onEnterSpy = sinon.spy();
+        var scrollListener = new ScrollListener({
+            el: innerEl,
+            container: outerEl,
+            onEnter: onEnterSpy
+        });
+
+        outerEl.scrollTop = 301; // inner element is showing at 150 (outer element scrollTop + outer element height)
+        // trigger scroll event
+        outerEl.dispatchEvent(TestUtils.createEvent('scroll'));
+        // trigger animation frame
+        requestAnimationFrameStub.yield();
+        assert.equal(onEnterSpy.callCount, 1, 'onEnter callback was fired when outer element is scrolled to point where top of inner element is showing');
         document.body.removeChild(outerEl);
     });
 });
