2e12b05dcb2cfbe9a4cb013932b8d7cf1b52be46
Mark <mkay581@gmail.com>
2016-03-17 16:26:39 -0400

    1.0.0

--- dist/scroll.js
+++ dist/scroll.js
@@ -1,5 +1,5 @@
 /** 
-* scroll-js - v0.5.2.
+* scroll-js - v1.0.0.
 * https://github.com/mkay581/scroll-js.git
 * Copyright 2016 Mark Kennedy. Licensed MIT.
 */
@@ -867,41 +867,100 @@ rawAsap.makeRequestCallFromTimer = makeRequestCallFromTimer;
 },{}],11:[function(require,module,exports){
 'use strict';
 
-var Promise = require('promise');
-var animationFramePolyFill = require('./utils/request-anim-polyfill');
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _promise = require('promise');
+
+var _promise2 = _interopRequireDefault(_promise);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+/**
+ * Gets an easing function based on supplied easing string.
+ * @param {String} easing - The easing id
+ * @returns {Function} - Returns the easing function
+ */
+var getEasing = function getEasing(easing) {
+    var defaultEasing = 'linear',
+        easeFunc = animMap[easing || defaultEasing];
+    if (!easeFunc) {
+        console.warn('Scroll error: scroller does not support an easing option of ' + easing + '. Using "' + defaultEasing + '" instead');
+        easeFunc = animMap[easing];
+    }
+    return easeFunc;
+};
+
+/**
+ * Map to hold easing functions.
+ * @type {WeakMap}
+ */
+var animMap = {
+    linear: function linear(t) {
+        return t;
+    },
+    easeInQuad: function easeInQuad(t) {
+        return t * t;
+    },
+    easeOutQuad: function easeOutQuad(t) {
+        return t * (2 - t);
+    },
+    easeInOutQuad: function easeInOutQuad(t) {
+        return t < .5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
+    },
+    easeInCubic: function easeInCubic(t) {
+        return t * t * t;
+    },
+    easeOutCubic: function easeOutCubic(t) {
+        return --t * t * t + 1;
+    },
+    easeInOutCubic: function easeInOutCubic(t) {
+        return t < .5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1;
+    },
+    easeInQuart: function easeInQuart(t) {
+        return t * t * t * t;
+    },
+    easeOutQuart: function easeOutQuart(t) {
+        return 1 - --t * t * t * t;
+    },
+    easeInOutQuart: function easeInOutQuart(t) {
+        return t < .5 ? 8 * t * t * t * t : 1 - 8 * --t * t * t * t;
+    },
+    easeInQuint: function easeInQuint(t) {
+        return t * t * t * t * t;
+    },
+    easeOutQuint: function easeOutQuint(t) {
+        return 1 + --t * t * t * t * t;
+    },
+    easeInOutQuint: function easeInOutQuint(t) {
+        return t < .5 ? 16 * t * t * t * t * t : 1 + 16 * --t * t * t * t * t;
+    }
+};
+
 /**
  * Scroll class.
  * @class Scroll
  * @param {object} options - Options to pass
  * @param {HTMLElement} options.el - The element to apply scroll to
  */
-var Scroll = function Scroll(options) {
-    this.initialize(options);
-};
-Scroll.prototype = {
+
+var Scroll = function () {
 
     /**
      * When the scroll is instantiated.
-     * @memberOf Scroll
+     * @param {Object} options - The initial options
+     * @param {HTMLElement} options.el - The element to scroll (the viewport)
      */
-    initialize: function initialize(options) {
+
+    function Scroll(options) {
+        _classCallCheck(this, Scroll);
 
         this.options = options;
-
         if (!options.el) {
             console.error('Scroll error: element passed to Scroll constructor is ' + options.el + '! Bailing...');
         }
-
-        this.setup();
-    },
-
-    /**
-     * Sets up the polyfill for requestAnimationFrame/cancelAnimationFrame.
-     * @memberOf Scroll
-     */
-    setup: function setup() {
-        animationFramePolyFill();
-    },
+    }
 
     /**
      * Scrolls the element until it's scroll properties match the coordinates provided.
@@ -910,216 +969,132 @@ Scroll.prototype = {
      * @param {Object} [options] - Scroll options
      * @param {Number} [options.duration]- The amount of time for the animation
      * @param {string} [options.easing] - The easing function to use
-     * @memberOf Scroll
      * @return {Promise}
      */
-    to: function to(x, y, options) {
-        var elem = this.options.el,
-            fromY = elem.scrollTop,
-            fromX = elem.scrollLeft;
-        // defaults
-        options = options || {};
-        options.duration = options.duration || 400;
 
-        return new Promise(function (resolve) {
-            this._scroll(elem, fromY, y, 'scrollTop', Date.now(), options.duration, this._getEasing(options.easing), resolve);
-        }.bind(this));
-    },
 
-    /**
-     * Returns document element
-     * @returns {HTMLDocument}
-     */
-    getDocumentElement: function getDocumentElement() {
-        return document;
-    },
+    _createClass(Scroll, [{
+        key: 'to',
+        value: function to(x, y, options) {
+            var _this = this;
 
-    /**
-     * Scroll to an element.
-     * @param {HTMLElement} el - The element to scroll to.
-     * @param {Object} [options] - The scroll options
-     */
-    toElement: function toElement(el, options) {
-        var container = this.options.el,
-            currentContainerScrollYPos = 0,
-            elementScrollYPos = el ? el.offsetTop : 0,
-            errorMsg;
+            var elem = this.options.el,
+                fromY = elem.scrollTop,
+                fromX = elem.scrollLeft;
+            // defaults
+            options = options || {};
+            options.duration = options.duration || 400;
 
-        // if the container is the document body or document itself, we'll
-        // need a different set of coordinates for accuracy
-        if (container === document.body) {
-            // using pageYOffset for cross-browser compatibility
-            currentContainerScrollYPos = window.pageYOffset;
-            // must add containers scroll y position to ensure an absolute value that does not change
-            elementScrollYPos = el.getBoundingClientRect().top + currentContainerScrollYPos;
+            /**
+             * Sets element's property to a value.
+             * @param {string} prop - The property to set
+             * @param {Number} value - The number value
+             */
+            var moveElement = function moveElement(prop, value) {
+                var el = _this.options.el;
+                el[prop] = value;
+                // scroll the html element also for cross-browser compatibility
+                // (ie. silly browsers like IE who need the html element to scroll too)
+                if (el === _this.document.body) {
+                    _this.document.documentElement[prop] = value;
+                }
+            };
+
+            /**
+             * Does a bit of calculating and scrolls an element.
+             * @param {HTMLElement} el - The element to be scrolled
+             * @param {Number} from - The number of where to scroll from
+             * @param {Number} to - The number of where to scroll to
+             * @param {string} prop - The property to animate
+             * @param {Number} startTime - The timestamp of when the animation should start
+             * @param {Number} duration - The amount of time for the animation
+             * @param {Function} easeFunc - The easing function to use
+             * @param [callback]
+             */
+            var scroll = function scroll(el, from, to, prop, startTime, duration, easeFunc, callback) {
+                window.requestAnimationFrame(function () {
+                    var currentTime = Date.now(),
+                        time = Math.min(1, (currentTime - startTime) / duration);
+
+                    if (from === to) {
+                        return callback ? callback() : null;
+                    }
+
+                    moveElement(prop, easeFunc(time) * (to - from) + from);
+
+                    /* prevent scrolling, if already there, or at end */
+                    if (time < 1) {
+                        scroll(el, el[prop], to, prop, startTime, duration, easeFunc, callback);
+                    } else if (callback) {
+                        callback();
+                    }
+                });
+            };
+
+            return new _promise2.default(function (resolve) {
+                scroll(elem, fromY, y, 'scrollTop', Date.now(), options.duration, getEasing(options.easing), resolve);
+            });
         }
-        if (!el) {
-            errorMsg = 'The element passed to Scroll.toElement() was undefined';
-            console.error(errorMsg);
-            return Promise.reject(new Error(errorMsg));
-        } else if (!container.contains(el)) {
-            errorMsg = 'Scroll.toElement() was passed an element that does not exist inside the scroll container';
-            console.warn(errorMsg);
-            return Promise.reject(new Error(errorMsg));
-        } else {
-            return this.to(0, elementScrollYPos, options);
-        }
-    },
 
-    /**
-     * Does a bit of calculating and scrolls an element.
-     * @param {HTMLElement} el - The element to be scrolled
-     * @param {Number} from - The number of where to scroll from
-     * @param {Number} to - The number of where to scroll to
-     * @param {string} prop - The property to animate
-     * @param {Number} startTime - The timestamp of when the animation should start
-     * @param {Number} duration - The amount of time for the animation
-     * @param {Function} easeFunc - The easing function to use
-     * @param [callback]
-     * @private
-     * @memberOf Scroll
-     */
-    _scroll: function _scroll(el, from, to, prop, startTime, duration, easeFunc, callback) {
-        requestAnimationFrame(function () {
-            var currentTime = Date.now(),
-                time = Math.min(1, (currentTime - startTime) / duration);
+        /**
+         * Returns document element
+         * @returns {HTMLDocument}
+         */
 
-            if (from === to) {
-                return callback ? callback() : null;
+    }, {
+        key: 'toElement',
+
+
+        /**
+         * Scroll to an element.
+         * @param {HTMLElement} el - The element to scroll to.
+         * @param {Object} [options] - The scroll options
+         */
+        value: function toElement(el, options) {
+            var container = this.options.el,
+                currentContainerScrollYPos = 0,
+                elementScrollYPos = el ? el.offsetTop : 0,
+                errorMsg;
+
+            // if the container is the document body or document itself, we'll
+            // need a different set of coordinates for accuracy
+            if (container === document.body) {
+                // using pageYOffset for cross-browser compatibility
+                currentContainerScrollYPos = window.pageYOffset;
+                // must add containers scroll y position to ensure an absolute value that does not change
+                elementScrollYPos = el.getBoundingClientRect().top + currentContainerScrollYPos;
             }
-
-            this._moveElement(prop, easeFunc(time) * (to - from) + from);
-
-            /* prevent scrolling, if already there, or at end */
-            if (time < 1) {
-                this._scroll(el, el[prop], to, prop, startTime, duration, easeFunc, callback);
-            } else if (callback) {
-                callback();
+            if (!el) {
+                errorMsg = 'The element passed to Scroll.toElement() was undefined';
+                console.error(errorMsg);
+                return _promise2.default.reject(new Error(errorMsg));
+            } else if (!container.contains(el)) {
+                errorMsg = 'Scroll.toElement() was passed an element that does not exist inside the scroll container';
+                console.warn(errorMsg);
+                return _promise2.default.reject(new Error(errorMsg));
+            } else {
+                return this.to(0, elementScrollYPos, options);
             }
-        }.bind(this));
-    },
-
-    /**
-     * Sets element's property to a value.
-     * @param {string} prop - The property to set
-     * @param {Number} value - The number value
-     * @private
-     */
-    _moveElement: function _moveElement(prop, value) {
-        var el = this.options.el;
-
-        el[prop] = value;
-
-        // scroll the html element also for cross-browser compatibility
-        // (ie. silly browsers like IE who need the html element to scroll too)
-        if (el === this.getDocumentElement().body) {
-            this.getDocumentElement().documentElement[prop] = value;
         }
-    },
 
-    /**
-     * Gets an easing function based on supplied easing string.
-     * @param {String} easing - The easing id
-     * @returns {Function} - Returns the easing function
-     * @private
-     * @memberOf Scroll
-     */
-    _getEasing: function _getEasing(easing) {
-        var defaultEasing = 'linear',
-            easeFunc = this._easing[easing || defaultEasing];
-        if (!easeFunc) {
-            console.warn('Scroll error: scroller does not support an easing option of ' + easing + '. Using "' + defaultEasing + '" instead');
-            easeFunc = this._easing[easing];
+        /**
+         * Use this to clean up the DOM when done.
+         */
+
+    }, {
+        key: 'destroy',
+        value: function destroy() {}
+    }, {
+        key: 'document',
+        get: function get() {
+            return document;
         }
-        return easeFunc;
-    },
+    }]);
 
-    /**
-     * Use this to clean up the DOM when done.
-     * @memberOf Scroll
-     */
-    destroy: function destroy() {},
-
-    /**
-     * Easing functions.
-     */
-    _easing: {
-        linear: function linear(t) {
-            return t;
-        },
-        easeInQuad: function easeInQuad(t) {
-            return t * t;
-        },
-        easeOutQuad: function easeOutQuad(t) {
-            return t * (2 - t);
-        },
-        easeInOutQuad: function easeInOutQuad(t) {
-            return t < .5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
-        },
-        easeInCubic: function easeInCubic(t) {
-            return t * t * t;
-        },
-        easeOutCubic: function easeOutCubic(t) {
-            return --t * t * t + 1;
-        },
-        easeInOutCubic: function easeInOutCubic(t) {
-            return t < .5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1;
-        },
-        easeInQuart: function easeInQuart(t) {
-            return t * t * t * t;
-        },
-        easeOutQuart: function easeOutQuart(t) {
-            return 1 - --t * t * t * t;
-        },
-        easeInOutQuart: function easeInOutQuart(t) {
-            return t < .5 ? 8 * t * t * t * t : 1 - 8 * --t * t * t * t;
-        },
-        easeInQuint: function easeInQuint(t) {
-            return t * t * t * t * t;
-        },
-        easeOutQuint: function easeOutQuint(t) {
-            return 1 + --t * t * t * t * t;
-        },
-        easeInOutQuint: function easeInOutQuint(t) {
-            return t < .5 ? 16 * t * t * t * t * t : 1 + 16 * --t * t * t * t * t;
-        }
-    }
-
-};
+    return Scroll;
+}();
 
 module.exports = Scroll;
 
-},{"./utils/request-anim-polyfill":12,"promise":1}],12:[function(require,module,exports){
-'use strict';
-
-module.exports = function () {
-    var x = 0,
-        lastTime = 0,
-        vendors = ['ms', 'moz', 'webkit', 'o'];
-
-    for (0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-        window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
-        window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] || window[vendors[x] + 'CancelRequestAnimationFrame'];
-    }
-
-    if (!window.requestAnimationFrame) {
-        window.requestAnimationFrame = function (callback, element) {
-            var currTime = new Date().getTime();
-            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-            var id = window.setTimeout(function () {
-                callback(currTime + timeToCall);
-            }, timeToCall);
-            lastTime = currTime + timeToCall;
-            return id;
-        };
-    }
-
-    if (!window.cancelAnimationFrame) {
-        window.cancelAnimationFrame = function (id) {
-            clearTimeout(id);
-        };
-    }
-};
-
-},{}]},{},[11])(11)
+},{"promise":1}]},{},[11])(11)
 });
