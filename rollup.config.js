8e7c97541706469a0b7be52e0f618e46fc8eb8d9
Mark <mark@heyimmark.com>
2018-11-11 17:18:24 -0500

    Abandon uglifyjs in favor of terser
    This fixes an issue where the minified file was not being properly
    created and caused issues with its usage in examples.

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,31 @@
 import typescript from 'rollup-plugin-typescript';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
 
-export default {
-    input: 'src/scroll.ts',
-    output: {
-        format: 'esm',
-        file: 'dist/scroll.js'
+export default [
+    {
+        input: 'src/scroll.ts',
+        output: {
+            format: 'esm',
+            file: 'dist/scroll.js'
+        },
+        plugins: [resolve(), typescript(), commonjs()],
+        watch: {
+            include: 'src/**'
+        }
     },
-    plugins: [resolve(), typescript(), commonjs()],
-    watch: {
-        include: 'src/**'
+    {
+        input: 'dist/scroll.js',
+        output: {
+            format: 'esm',
+            file: 'dist/scroll.min.js'
+        },
+        plugins: [
+            terser({
+                compress: true,
+                mangle: true
+            })
+        ]
     }
-};
+];
