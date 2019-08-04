b2fd0aa88d5d1e03ac34e695a1b0da0bfaa152de
Mark <mark@heyimmark.com>
2019-08-04 18:22:01 -0400

    Add release-it

---
+++ .release-it.js
@@ -0,0 +1,19 @@
+module.exports = {
+    git: {
+        commitMessage: '${version}',
+        requireCleanWorkingDir: false,
+        requireUpstream: false,
+        tagName: 'v${version}',
+    },
+    github: {
+        release: true,
+        releaseName: '${version}',
+        releaseNotes: null,
+    },
+    hooks: {
+        'before:init': ['npm test'],
+        'after:bump': 'npm run build-dist && npm run banner',
+        'after:release':
+            'echo Successfully released ${name} v${version} to ${repo.repository}.',
+    },
+};
