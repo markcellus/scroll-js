5e0500843e5b56b6d8f78378af5e115e92316518
Mark <mark@heyimmark.com>
2019-08-18 18:21:35 -0400

    Remove rollup-plugin-node-resolve

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,8 @@
 import typescript from 'rollup-plugin-typescript2';
-import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
 
-const basePlugins = [resolve(), typescript()];
+const basePlugins = [typescript()];
 const commonJSPlugins = [...basePlugins, commonjs()];
 
 export default [
