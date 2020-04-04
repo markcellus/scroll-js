228136f7386ee7075812aafdb9e7bc90bea33729
Mark <mark@heyimmark.com>
2020-04-04 10:08:45 -0400

    Update rollup plugins

--- rollup.config.js
+++ rollup.config.js
@@ -2,8 +2,9 @@ import typescript from 'rollup-plugin-typescript2';
 import { terser } from 'rollup-plugin-terser';
 import babel from 'rollup-plugin-babel';
 import serve from 'rollup-plugin-serve';
-import commonjs from 'rollup-plugin-commonjs';
-import resolve from 'rollup-plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+
 
 export default [
     {
@@ -44,11 +45,11 @@ export default [
                 mangle: true,
             }),
             process.env.ROLLUP_WATCH &&
-                serve({
-                    historyApiFallback: true,
-                    contentBase: '',
-                    port: 9383,
-                }),
+            serve({
+                historyApiFallback: true,
+                contentBase: '',
+                port: 9383,
+            }),
         ],
     },
 ];
